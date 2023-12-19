4a8270b8200bef7eb6c22dc376de388ac76263a7
lucidojohncarlo <lucidojohncarlo10@gmail.com>
2023-12-19 13:47:51 +0800

    12-19-2023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,48 @@
         <link href="{{ asset('css/Welcome.css') }}" rel="stylesheet">
 
     </head>
-    <body class="antialiased" >
-      <nav>
-        <div class="navbar-container">
-    <ul>
+    <body>
+
+        <!-- ======= Header ======= -->
+        <header id="header" class="header fixed-top d-flex align-items-center">
+          <div class="container d-flex align-items-center justify-content-between">
       
-      {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"> --}}
+            <a href="{{ url()->previous() }}" class="logo d-flex align-items-center me-auto me-lg-0">
+              <!-- Uncomment the line below if you also wish to use an image logo -->
+              <!-- <img src="assets/img/logo.png" alt=""> -->
+              <h1>Philippine Society for Quality<span>.</span></h1>
+            </a>
+      
+            <nav id="navbar" class="navbar">
+              <ul>
+                
+                <li><a href="{{ url()->previous() }}">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#events">Events</a></li>
+                <li><a href="#chefs">Chefs</a></li>
+                <li><a href="#gallery">Gallery</a></li>
+               
+      
+            <a class="btn-book-a-table" href="{{ route('login') }}">Login</a>
+            <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+            <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+            @if (Route::has('register'))        
+            <a class="btn-book-a-table" href="{{ route('register') }}">Register</a>
+            <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+            <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+            @endif
+            
+          </div>
+        
+              </ul>
+            </nav>
+          </div>
+        </header><!-- End Header -->
+   
+    {{-- <ul>
+      
+      <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"> 
         @if (Route::has('login'))
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                 @auth
@@ -35,9 +71,26 @@
         @endif
     </ul>
         </div>
-      </nav>
-      <img src="/img/Psq.png" alt="">
-      
+      </nav> --}}
+      {{-- <img src="/img/Psq.png" alt=""> --}}
+       <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center section-bg">
+    <div class="container">
+      <div class="row justify-content-between gy-5">
+        <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
+          <h2 data-aos="fade-up">Welcome PSQ Nations<br>Grow in Exelence with Us</h2>
+          <p data-aos="fade-up" data-aos-delay="100">Philippine Society for Quality * Since 1969</p>
+          <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+          </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2 text-center text-lg-start">
+          {{-- <img src="/img/Psq.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="300"> --}}
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero Section -->
+
+
                 </div>
             </div>
         </div>
