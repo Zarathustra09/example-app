faf4e7c3c78dbaba4341e0167c46ccd513ea746a
lucidojohncarlo <lucidojohncarlo10@gmail.com>
2023-12-19 13:54:40 +0800

    12-19-2023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,22 @@
         <title>Philippine Society for Quality</title>
     
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
+        <!-- Favicons -->
+        <link href="assets/img/favicon.png" rel="icon">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+      
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Amatic+SC:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+      
+        <!-- Vendor CSS Files -->
+        <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('vendor/aos/aos.css" rel="stylesheet') }}">
+        <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+      
         <link href="{{ asset('css/Welcome.css') }}" rel="stylesheet">
 
     </head>
